afef062c0414670fc6fecaa1b0ff1ca8b01127c0
alex <12alexaci12@gmail.com>
2020-11-21 10:35:51 -0400

    ficha tecnica de proveedores

---
+++ privada/reportes/templates_c/aa48cb3073cfe92bd362f64a37a00722dc1cfe55_0.file.fichas_tec_proveedores1.tpl.php
@@ -0,0 +1,98 @@
+<?php
+/* Smarty version 3.1.29, created on 2020-11-21 14:02:34
+  from "D:\disenoWeb\www\sis_aluformas\privada\reportes\templates\fichas_tec_proveedores1.tpl" */
+
+if ($_smarty_tpl->smarty->ext->_validateCompiled->decodeProperties($_smarty_tpl, array (
+  'has_nocache_code' => false,
+  'version' => '3.1.29',
+  'unifunc' => 'content_5fb91dfa9ac839_09408051',
+  'file_dependency' => 
+  array (
+    'aa48cb3073cfe92bd362f64a37a00722dc1cfe55' => 
+    array (
+      0 => 'D:\\disenoWeb\\www\\sis_aluformas\\privada\\reportes\\templates\\fichas_tec_proveedores1.tpl',
+      1 => 1605966371,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5fb91dfa9ac839_09408051 ($_smarty_tpl) {
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+     <head>
+          <meta charset="utf-8">
+          <?php echo '<script'; ?>
+ type="text/javascript">
+               var ventanaCalendario = false;
+               function imprimir(){
+                    if (confirm(' Desea Imprimir?')) {
+                         window.print();
+                    }
+               }
+          <?php echo '</script'; ?>
+>
+     </head>
+     <body style="cursor:pointer;cursor:hand" onclick="imprimir();">
+          <table width="100%" border="0">
+               <tr>
+                    <td>
+                         <img src="../../img/bitmap1.svg" width="70%">
+                    </td>
+                    <td align="center" width="80%">
+                         <h2>FICHA TECNICA DE PROVEEDORES</h2>
+                    </td>
+               </tr>
+          </table>
+          <br>
+          <center>
+               <table border="1">
+                    <tr>
+                         <td>
+                              <table border="0">
+                              <?php
+$_from = $_smarty_tpl->tpl_vars['proveedores']->value;
+if (!is_array($_from) && !is_object($_from)) {
+settype($_from, 'array');
+}
+$__foreach_r_0_saved_item = isset($_smarty_tpl->tpl_vars['r']) ? $_smarty_tpl->tpl_vars['r'] : false;
+$_smarty_tpl->tpl_vars['r'] = new Smarty_Variable();
+$_smarty_tpl->tpl_vars['r']->_loop = false;
+foreach ($_from as $_smarty_tpl->tpl_vars['r']->value) {
+$_smarty_tpl->tpl_vars['r']->_loop = true;
+$__foreach_r_0_saved_local_item = $_smarty_tpl->tpl_vars['r'];
+?>
+                                   <tr>
+                                        <th align="right">Nombre</th><th>:</th>
+                                        <td> <input type="text" name="ci" value="<?php echo $_smarty_tpl->tpl_vars['r']->value['nombre'];?>
+" disabled=""> </td>
+                                   </tr>
+                                   <tr>
+                                        <th align="right">direccion</th><th>:</th>
+                                        <td> <input type="text" name="direccion" value="<?php echo $_smarty_tpl->tpl_vars['r']->value['direccion'];?>
+" disabled=""> </td>
+                                   </tr>
+                                   <tr>
+                                        <th align="right">Telefono</th><th>:</th>
+                                        <td> <input type="text" name="ci" value="<?php echo $_smarty_tpl->tpl_vars['r']->value['telefono'];?>
+" disabled=""> </td>
+                                   </tr>
+                              <?php
+$_smarty_tpl->tpl_vars['r'] = $__foreach_r_0_saved_local_item;
+}
+if ($__foreach_r_0_saved_item) {
+$_smarty_tpl->tpl_vars['r'] = $__foreach_r_0_saved_item;
+}
+?>
+                              </table>
+                         </td>
+                    </tr>
+               </table>
+          </center>
+     </body>
+</html>
+<?php }
+}
